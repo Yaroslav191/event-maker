150015f7880d512e3261a3e9eeee4aedb9c0f4ed
Yaroslav <yaroslavv86@gmail.com>
2024-03-01 17:40:11 +0500

    fix

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,17 +4,7 @@ import EditScreenInfo from "@/src/components/EditScreenInfo";
 import { Text, View } from "@/src/components/Themed";
 
 export default function TabOneScreen() {
-   return (
-      <View style={styles.container}>
-         <Text style={styles.title}>Hello World</Text>
-         <View
-            style={styles.separator}
-            lightColor="#eee"
-            darkColor="rgba(255,255,255,0.1)"
-         />
-         <EditScreenInfo path="app/(tabs)/index.tsx" />
-      </View>
-   );
+   return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
