08a15db7faff0411fdb1f31c2a18fd41c1c8732c
Yaroslav <yaroslavv86@gmail.com>
2024-03-28 00:05:05 +0500

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,35 @@
 import axios from "axios";
 import { MarkerInterface } from "../types";
+import * as Location from "expo-location";
 
-interface  FetchMarkersInterface {
-   setMarkers: (merker: MarkerInterface) => void
+interface FetchMarkersInterface {
+  setMarkers: (merker: MarkerInterface) => void;
 }
 
-export const fetchMarkers = async (setMarkers : FetchMarkersInterface['setMarkers'])   => {
-   try {
-      const response = await axios.get("http://192.168.0.106:8000/markers");
+export const fetchMarkers = async (
+  setMarkers: FetchMarkersInterface["setMarkers"]
+) => {
+  try {
+    const response = await axios.get("http://192.168.100.123:8000/markers");
 
-      if (response.status === 200) {
-         setMarkers(response.data);
-      }
-   } catch (error) {
-      console.log(error);
-   } finally {
-      // setLoading(false);
-   }
+    if (response.status === 200) {
+      setMarkers(response.data);
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    // setLoading(false);
+  }
+};
+
+export const reverseGeocode = async (longitude, latitude) => {
+  const reverseGeocodedAddress = await Location.reverseGeocodeAsync({
+    longitude,
+    latitude,
+  });
+
+  console.log("Reverse Geocoded:");
+  console.log(reverseGeocodedAddress);
+
+  return reverseGeocodedAddress;
 };
