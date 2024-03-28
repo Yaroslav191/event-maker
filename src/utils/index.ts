2c04e9f892016bde80953779c975608f164d16a9
Yaroslav <yaroslavv86@gmail.com>
2024-03-28 17:58:13 +0500

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { MarkerInterface } from "../types";
-import * as Location from "expo-location";
+// import * as Location from "expo-location";
 
 interface FetchMarkersInterface {
   setMarkers: (merker: MarkerInterface) => void;
@@ -10,7 +10,7 @@ export const fetchMarkers = async (
   setMarkers: FetchMarkersInterface["setMarkers"]
 ) => {
   try {
-    const response = await axios.get("http://192.168.100.123:8000/markers");
+    const response = await axios.get("http://192.168.0.108:8000/markers");
 
     if (response.status === 200) {
       setMarkers(response.data);
@@ -22,14 +22,14 @@ export const fetchMarkers = async (
   }
 };
 
-export const reverseGeocode = async (longitude, latitude) => {
-  const reverseGeocodedAddress = await Location.reverseGeocodeAsync({
-    longitude,
-    latitude,
-  });
+// export const reverseGeocode = async (longitude, latitude) => {
+//   const reverseGeocodedAddress = await Location.reverseGeocodeAsync({
+//     longitude,
+//     latitude,
+//   });
 
-  console.log("Reverse Geocoded:");
-  console.log(reverseGeocodedAddress);
+//   console.log("Reverse Geocoded:");
+//   console.log(reverseGeocodedAddress);
 
-  return reverseGeocodedAddress;
-};
+//   return reverseGeocodedAddress;
+// };
