a5b1e2ecd2f2764515459ee63eef01a736444b51
Yaroslav <yaroslavv86@gmail.com>
2024-04-09 23:45:24 +0500

    fix

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,17 +1,41 @@
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
-import React from "react";
+import React, { useContext } from "react";
 import { Image, Text, View, StyleSheet, Pressable } from "react-native";
 import { useState } from "react";
+import MapView, { MapCalloutSubview, Marker } from "react-native-maps";
+import { MapType } from "../context/MapContext";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useRoute } from "@react-navigation/native";
 
 const EventDetailsScreen = () => {
-   const { id } = useLocalSearchParams();
+  const { id } = useLocalSearchParams();
+  const route = useRoute();
+  const { testvalue } = route.params;
+  const { currentLocation, setCurrentLocation } = useContext(MapType);
 
-   return (
-      <View>
-         <Stack.Screen options={{ title: "Details" + id }} />
-         <Text>{id}</Text>
+  console.log();
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      {/* <Stack.Screen options={{ title: "Details" + id }} /> */}
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ height: "35%" }}
+          initialRegion={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          <Marker
+          // coordinate={marker.latlng}
+          // title={marker.title}
+          // description={marker.description}
+          />
+        </MapView>
       </View>
-   );
+    </GestureHandlerRootView>
+  );
 };
 
 export default EventDetailsScreen;
--- src/context/MapContext.tsx
+++ src/context/MapContext.tsx
@@ -1,24 +1,37 @@
 import { createContext, useState } from "react";
 import { MarkerInterface } from "../types";
+import { LatLng } from "react-native-maps";
 
 export interface MapContextType {
-   markers: MarkerInterface[];
-   setMarkers: (markers: MarkerInterface[]) => void;
+  markers: MarkerInterface[];
+  setMarkers: (markers: MarkerInterface[]) => void;
+  currentLocation: LatLng;
+  setCurrentLocation: (currentLocation: LatLng) => void;
 }
 
 const MapType = createContext<MapContextType>({
-   markers: [],
-   setMarkers: () => {},
+  markers: [],
+  setMarkers: () => {},
+  currentLocation: {
+    latitude: 0,
+    longitude: 0,
+  },
+  setCurrentLocation: () => {},
 });
 
 const MapContext: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-   const [markers, setMarkers] = useState<MarkerInterface[]>([]);
+  const [markers, setMarkers] = useState<MarkerInterface[]>([]);
+  const [currentLocation, setCurrentLocation] = useState<LatLng>({
+    latitude: 0,
+    longitude: 0,
+  });
 
-   return (
-      <MapType.Provider value={{ markers, setMarkers }}>
-         {children}
-      </MapType.Provider>
-   );
+  return (
+    <MapType.Provider
+      value={{ markers, setMarkers, currentLocation, setCurrentLocation }}>
+      {children}
+    </MapType.Provider>
+  );
 };
 
 export { MapType, MapContext };
