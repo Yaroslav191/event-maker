5b7f9e2b403bfb2d5b8846ab8b7d6b000663fb26
Yaroslav <yaroslavv86@gmail.com>
2024-04-19 17:57:45 +0500

    fix

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,210 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+   View,
+   Text,
+   Pressable,
+   KeyboardAvoidingView,
+   Alert,
+   TextInput,
+} from "react-native";
+import React, { useState } from "react";
 import Button from "../components/Button";
-import { Link, Redirect } from "expo-router";
+import { Link, Redirect, useNavigation } from "expo-router";
 import Svg, { Image } from "react-native-svg";
 
-const index = () => {
-   return (
-      <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
-         <Link href={"/(use)"} asChild>
-            <Button text="User" />
-         </Link>
-         <Link href={"/(admin)"} asChild>
-            <Button text="Admin" />
-         </Link>
+import axios from "axios";
 
-         <Button text="Sign out" />
+const index = () => {
+   const [email, setEmail] = useState("");
+   const [name, setName] = useState("");
+   const [password, setPassword] = useState("");
+   const [image, setImage] = useState("");
+   const navigation = useNavigation();
+
+   const handleRegister = () => {
+      console.log("press");
+
+      const user = {
+         name: name,
+         email: email,
+         password: password,
+         image: image,
+      };
+
+      axios
+         .post("http://192.168.0.113:8000/register", user)
+         .then((response) => {
+            console.log(response);
+            Alert.alert(
+               "Registration successful",
+               "You have been registered Successfully"
+            );
+            setName("");
+            setEmail("");
+            setPassword("");
+            setImage("");
+         })
+         .catch((error) => {
+            Alert.alert(
+               "Registration Error",
+               "An error occurred while registering"
+            );
+            console.log("Registration failed", error);
+         });
+   };
+
+   return (
+      <View
+         style={{
+            flex: 1,
+            backgroundColor: "white",
+            padding: 10,
+            alignItems: "center",
+         }}
+      >
+         <KeyboardAvoidingView>
+            <View
+               style={{
+                  marginTop: 100,
+                  justifyContent: "center",
+                  alignItems: "center",
+               }}
+            >
+               <Text
+                  style={{ color: "#4A55A2", fontSize: 17, fontWeight: "600" }}
+               >
+                  Sign In
+               </Text>
+               <Text style={{ fontSize: 17, fontWeight: "600", marginTop: 15 }}>
+                  Register To Your Account
+               </Text>
+            </View>
+
+            <View style={{ marginTop: 50 }}>
+               <View>
+                  <Text
+                     style={{ fontSize: 18, fontWeight: "600", color: "gray" }}
+                  >
+                     Name
+                  </Text>
+                  <TextInput
+                     value={name}
+                     onChangeText={(text) => setName(text)}
+                     style={{
+                        fontSize: 18,
+                        borderBottomColor: "gray",
+                        borderBottomWidth: 1,
+                        marginVertical: 10,
+                        width: 300,
+                     }}
+                     placeholder="Enter your Name"
+                     placeholderTextColor={"black"}
+                  />
+               </View>
+               <View style={{ marginTop: 10 }}>
+                  <Text
+                     style={{ fontSize: 18, fontWeight: "600", color: "gray" }}
+                  >
+                     Email
+                  </Text>
+                  <TextInput
+                     value={email}
+                     onChangeText={(text) => setEmail(text)}
+                     style={{
+                        fontSize: 18,
+                        borderBottomColor: "gray",
+                        borderBottomWidth: 1,
+                        marginVertical: 10,
+                        width: 300,
+                     }}
+                     placeholder="Enter your email"
+                     placeholderTextColor={"black"}
+                  />
+               </View>
+
+               <View style={{ marginTop: 10 }}>
+                  <Text
+                     style={{ fontSize: 18, fontWeight: "600", color: "gray" }}
+                  >
+                     Password
+                  </Text>
+                  <TextInput
+                     value={password}
+                     onChangeText={(text) => setPassword(text)}
+                     secureTextEntry={true}
+                     style={{
+                        fontSize: 18,
+                        borderBottomColor: "gray",
+                        borderBottomWidth: 1,
+                        marginVertical: 10,
+                        width: 300,
+                     }}
+                     placeholder="Enter your Password"
+                     placeholderTextColor={"black"}
+                  />
+               </View>
+               <View style={{ marginTop: 10 }}>
+                  <Text
+                     style={{ fontSize: 18, fontWeight: "600", color: "gray" }}
+                  >
+                     Image
+                  </Text>
+                  <TextInput
+                     value={image}
+                     onChangeText={(text) => setImage(text)}
+                     style={{
+                        fontSize: 18,
+                        borderBottomColor: "gray",
+                        borderBottomWidth: 1,
+                        marginVertical: 10,
+                        width: 300,
+                     }}
+                     placeholder="Image"
+                     placeholderTextColor={"black"}
+                  />
+               </View>
+               <Pressable
+                  onPress={handleRegister}
+                  style={{
+                     marginTop: 30,
+                     width: 200,
+                     backgroundColor: "#4A55A2",
+                     padding: 15,
+                     marginLeft: "auto",
+                     marginRight: "auto",
+                     borderRadius: 6,
+                  }}
+               >
+                  <Text
+                     style={{
+                        color: "white",
+                        fontSize: 16,
+                        fontWeight: "bold",
+                        textAlign: "center",
+                     }}
+                  >
+                     Register
+                  </Text>
+               </Pressable>
+               <Pressable
+                  style={{
+                     marginTop: 15,
+                  }}
+               >
+                  <Text
+                     style={{
+                        color: "grey",
+                        textAlign: "center",
+                        fontSize: 16,
+                     }}
+                     onPress={() => {
+                        navigation.goBack();
+                     }}
+                  >
+                     Already Have an account? Sign in
+                  </Text>
+               </Pressable>
+            </View>
+         </KeyboardAvoidingView>
       </View>
    );
 };
